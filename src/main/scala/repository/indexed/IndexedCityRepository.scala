5a5706a95818ad2f16c30ad2fdb564425950e5b2
Mario Zeller <zellermario@me.com>
2024-09-23 00:53:28 +0200

    Exclude source city from results

--- src/main/scala/repository/indexed/IndexedCityRepository.scala
+++ src/main/scala/repository/indexed/IndexedCityRepository.scala
@@ -9,18 +9,15 @@ class IndexedCityRepository extends CityRepository {
 
   private var cities: Seq[City] = Seq.empty
   private val index: S2PointIndex[City] = new S2PointIndex[City]()
-  
-  private def toS2Point(city: City): S2Point = 
+
+  private def toS2Point(city: City): S2Point =
     S2LatLng.fromDegrees(
       city.coordinate.latitude.toDegrees,
       city.coordinate.longitude.toDegrees).toPoint
 
   override def loadCities(cities: Seq[City]): Unit = {
-    cities.foreach(city =>
-      val s2Point = toS2Point(city)
-      index.add(s2Point, city)
-    )
     this.cities = cities
+    cities.foreach(city => index.add(toS2Point(city), city))
   }
 
   override def getAllCities: Seq[City] = cities
@@ -30,10 +27,13 @@ class IndexedCityRepository extends CityRepository {
 
   override def findNearestCities(sourceCity: City, numberOfCities: Int): Seq[City] = {
     val query = S2ClosestPointQuery[City](index)
-    query.setMaxPoints(numberOfCities)
+    query.setMaxPoints(numberOfCities + 1) // Because the first result will be the source city itself
     val sourceS2Point = toS2Point(sourceCity)
     val results = query.findClosestPoints(sourceS2Point)
-    results.stream().map(result => result.entry().data()).toScala(Seq)
+    results.stream()
+      .map(result => result.entry().data())
+      .toScala(Seq)
+      .drop(1) // Drop the source city
   }
 
 }
